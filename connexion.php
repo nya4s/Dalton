dbc9b810df2060ead310c05fc254e9e9ccb35138
saboyg <saboy.gaston34@gmail.com>
2020-11-23 15:06:40 +0100

    *

--- connexion.php
+++ connexion.php
@@ -15,14 +15,21 @@ $email = $_GET['email'];
 $pwd = $_GET['pwd'];
 
 
-
 $reponse = $bdd->query('SELECT * FROM user') or die(print_r($bdd->errorInfo()));
 
 while($val = $reponse->fetch()){
 
-	echo $val['nom'] + '\n';
-	echo $val['prenom'];
+    echo $val['id'] . '<br>';
+	echo $val['nom'] . '<br>';
+	echo $val['prenom'] . '<br>';
+	echo $val['email'] . '<br>';
+	echo $val['pwd'] . '<br>';
+	echo $val['date_naissance'] . '<br>';
+	echo $val['date_incription'] . '<br>';
+
 }
+
+
 
 
 ?>
